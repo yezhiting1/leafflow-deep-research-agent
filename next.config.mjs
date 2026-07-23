922b76febdd7e92637b57a815aa8220e40990ef2
一叶知秋 <147503173+yezhiting1@users.noreply.github.com>
2026-07-23 11:16:59 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
-/** @type {import("next").NextConfig} */
+// /** @type {import("next").NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   turbopack: {
+//     root: ".",
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: "./",
+  basePath: "",
   reactStrictMode: true,
-  turbopack: {
-    root: ".",
-  },
+  // 腾讯云托管额外配置，放行静态资源转发
+  output: "standalone"
 };
 
 export default nextConfig;
-
